3bc81f0012af65bb493bf2d128f01ef9667a030f
Alissa Sabre <alissa_sabre@yahoo.co.jp>
2020-06-24 16:27:01 +0900

    I don't like instanciating a converter in Resource every time I need one.

---
+++ GTBench/BooleanToVisibilityConverterExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Markup;
+
+namespace GTBench
+{
+    [MarkupExtensionReturnType(typeof(BooleanToVisibilityConverter))]
+    public class BooleanToVisibilityConverterExtension : MarkupExtension
+    {
+        private static readonly BooleanToVisibilityConverter Instance = new BooleanToVisibilityConverter();
+        public override object ProvideValue(IServiceProvider serviceProvider) => Instance;
+    }
+}
